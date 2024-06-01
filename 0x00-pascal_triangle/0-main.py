3616353ace5cbc24645da4abc7696e95399a3104
ingchrist <cdjinguet@gmail.com>
2024-06-01 23:50:34 +0100

    files added

--- 0x00-pascal_triangle/0-main.py
+++ 0x00-pascal_triangle/0-main.py
@@ -14,3 +14,6 @@ def print_triangle(triangle):
 
 if __name__ == "__main__":
     print_triangle(pascal_triangle(5))
+
+
+                                                                             
