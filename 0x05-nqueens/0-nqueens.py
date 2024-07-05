56d5b3ba2fbd3b908b70ac7421c88816ae509da5
ingchrist <cdjinguet@gmail.com>
2024-07-05 23:03:36 +0100

    ok

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -89,3 +89,11 @@ if __name__ == '__main__':
             print(val, end='')
         else:
             print(val)
+
+                   
+
+                       
+
+                           
+
+
